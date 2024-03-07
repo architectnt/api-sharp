bfe5c8d6189a0edecfa757b3aab9bf6ad7039bf0
architectnt <eno@i2iproductions.com>
2024-03-07 00:32:44 -0800

    update aafc wrapper to support exporting and aafc 2.1 features

--- src/aafc_wrapper/ArchitectAudioClipFormat.cs
+++ src/aafc_wrapper/ArchitectAudioClipFormat.cs
@@ -11,7 +11,7 @@ namespace ArchitectAPI.Wrappers.Audio
         /// <param name="data">The managed data to utilize</param>
         /// <returns>Uncompressed 32 bit float AAFC data</returns>
         [DllImport("aafc")]
-        public static extern IntPtr aafc_import(byte[] data);
+        public static extern IntPtr aafc_import(byte* data);
 
         /// <summary>
         /// Native AAFC Export
@@ -23,7 +23,7 @@ namespace ArchitectAPI.Wrappers.Audio
         /// <param name="bps">Bits per sample</param>
         /// <param name="sampletype">1 -> PCM, 2 -> ADPCM</param>
         [DllImport("aafc")]
-        public static extern IntPtr aafc_export(float[] samples, int freq, int channels, int samplelength, byte bps = 16, byte sampletype = 1, bool forcemono = false);
+        public static extern IntPtr aafc_export(float* samples, int freq, int channels, int samplelength, byte bps = 16, byte sampletype = 1, bool forcemono = false);
 
         /// <summary>
         /// Gets the header from AAFC data
@@ -31,14 +31,21 @@ namespace ArchitectAPI.Wrappers.Audio
         /// <param name="data">AAFC Input</param>
         /// <returns>28 byte header or 12 byte legacy header</returns>
         [DllImport("aafc")]
-        public static extern IntPtr aafc_getheader(byte[] data);
+        public static extern IntPtr aafc_getheader(byte* data);
+
+        /// <summary>
+        /// Frees all AAFC imported data from memory
+        /// </summary>
+        /// <param name="arr"></param>
+        [DllImport("aafc")]
+        public static extern IntPtr aafc_free(float* arr);
 
         /// <summary>
         /// Frees all AAFC data from memory
         /// </summary>
         /// <param name="arr"></param>
         [DllImport("aafc")]
-        public static extern IntPtr aafc_free(IntPtr arr);
+        public static extern IntPtr aafc_free_bytes(byte* arr);
 
         /// <summary>
         /// Natively convert floating point arrays to integer types
@@ -49,6 +56,16 @@ namespace ArchitectAPI.Wrappers.Audio
         /// <returns></returns>
         [DllImport("aafc")]
         public static extern IntPtr aafc_float_to_int(float* arr, long length, byte type)
+
+        /// <summary>
+        /// Natively convert floating point arrays to integer types
+        /// </summary>
+        /// <param name="arr">The input array</param>
+        /// <param name="length">The absolute samplelength</param>
+        /// <param name="type">Integer type (8 > byte, 16 > short, 32 > int)</param>
+        /// <returns></returns>
+        [DllImport("aafc")]
+        public static extern IntPtr aafc_int_to_float(IntPtr* arr, long length, byte type)
 
         /// <summary>
         /// Resample audio
@@ -79,39 +96,208 @@ namespace ArchitectAPI.Wrappers.Audio
             public byte sampletype;
         }
 
+        public unsafe static byte[] ToByteArray(float[] samples, int channels, int samplerate, bool mono = false, byte bps = 16, byte sampletype = 1, int sproverride = 0)
+        {
+            byte* rstptr = (byte*)aafc_export(samples, clip.frequency, clip.channels, samples.Length, bps, sampletype, mono, sproverride);
+
+            int splen = (mono ? (samples.Length / channels) : samples.Length);
+            float reratio = sproverride != 0 ? (float)sproverride / clip.frequency : 0;
+            int resampledlen = (int)(splen * reratio);
+
+            int fnsplen = sproverride != 0 ? resampledlen : splen;
+
+            int bsize = bps switch
+            {
+                4 => fnsplen / 2,
+                8 => fnsplen * sizeof(byte),
+                10 => ((fnsplen * 5 + 3) / 4),
+                12 => ((fnsplen * 3) / 2),
+                16 => fnsplen * sizeof(short),
+                24 => fnsplen * 3,
+                32 => fnsplen * sizeof(float),
+                _ => 0
+            };
+
+            int bitm = sampletype switch
+            {
+                1 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                2 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 2,
+                3 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 8,
+                4 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                _ => 0,
+            };
+
+            byte[] rst = new byte[bitm];
+
+            Marshal.Copy((IntPtr)rstptr, rst, 0, bitm);
+            Marshal.FreeHGlobal((IntPtr)rstptr);
+            return rst;
+        }
+
+        public unsafe static byte[] ToByteArray(byte[] isamples, int channels, int samplerate, bool mono = false, byte bps = 16, byte sampletype = 1, int sproverride = 0)
+        {
+            float[] samples;
+            fixed (byte* ptr = isamples)
+            {
+                Marshal.Copy((IntPtr)aafc_int_to_float(isamples, isamples.Length, 8), samples, 0, isamples.Length);
+            }
+
+            byte* rstptr = (byte*)aafc_export(samples, clip.frequency, clip.channels, samples.Length, bps, sampletype, mono, sproverride);
+
+            int splen = (mono ? (samples.Length / channels) : samples.Length);
+            float reratio = sproverride != 0 ? (float)sproverride / clip.frequency : 0;
+            int resampledlen = (int)(splen * reratio);
+
+            int fnsplen = sproverride != 0 ? resampledlen : splen;
+
+            int bsize = bps switch
+            {
+                4 => fnsplen / 2,
+                8 => fnsplen * sizeof(byte),
+                10 => ((fnsplen * 5 + 3) / 4),
+                12 => ((fnsplen * 3) / 2),
+                16 => fnsplen * sizeof(short),
+                24 => fnsplen * 3,
+                32 => fnsplen * sizeof(float),
+                _ => 0
+            };
+
+            int bitm = sampletype switch
+            {
+                1 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                2 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 2,
+                3 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 8,
+                4 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                _ => 0,
+            };
+
+            byte[] rst = new byte[bitm];
+
+            Marshal.Copy((IntPtr)rstptr, rst, 0, bitm);
+            Marshal.FreeHGlobal((IntPtr)rstptr);
+            return rst;
+        }
+
+        public unsafe static byte[] ToByteArray(short[] isamples, int channels, int samplerate, bool mono = false, byte bps = 16, byte sampletype = 1, int sproverride = 0)
+        {
+            float[] samples;
+            fixed (short* ptr = isamples)
+            { 
+                Marshal.Copy((IntPtr)aafc_int_to_float(isamples, isamples.Length, 16), samples, 0, isamples.Length);
+            }
+
+            byte* rstptr = (byte*)aafc_export(samples, clip.frequency, clip.channels, samples.Length, bps, sampletype, mono, sproverride);
+
+            int splen = (mono ? (samples.Length / channels) : samples.Length);
+            float reratio = sproverride != 0 ? (float)sproverride / clip.frequency : 0;
+            int resampledlen = (int)(splen * reratio);
+
+            int fnsplen = sproverride != 0 ? resampledlen : splen;
+
+            int bsize = bps switch
+            {
+                4 => fnsplen / 2,
+                8 => fnsplen * sizeof(byte),
+                10 => ((fnsplen * 5 + 3) / 4),
+                12 => ((fnsplen * 3) / 2),
+                16 => fnsplen * sizeof(short),
+                24 => fnsplen * 3,
+                32 => fnsplen * sizeof(float),
+                _ => 0
+            };
+
+            int bitm = sampletype switch
+            {
+                1 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                2 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 2,
+                3 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 8,
+                4 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                _ => 0,
+            };
+
+            byte[] rst = new byte[bitm];
+
+            Marshal.Copy((IntPtr)rstptr, rst, 0, bitm);
+            Marshal.FreeHGlobal((IntPtr)rstptr);
+            return rst;
+        }
+
+        public unsafe static byte[] ToByteArray(int[] isamples, int channels, int samplerate, bool mono = false, byte bps = 16, byte sampletype = 1, int sproverride = 0)
+        {
+            float[] samples;
+            fixed (int* ptr = isamples)
+            {
+                Marshal.Copy((IntPtr)aafc_int_to_float(isamples, isamples.Length, 32), samples, 0, isamples.Length);
+            }
+
+            byte* rstptr = (byte*)aafc_export(samples, clip.frequency, clip.channels, samples.Length, bps, sampletype, mono, sproverride);
+
+            int splen = (mono ? (samples.Length / channels) : samples.Length);
+            float reratio = sproverride != 0 ? (float)sproverride / clip.frequency : 0;
+            int resampledlen = (int)(splen * reratio);
+
+            int fnsplen = sproverride != 0 ? resampledlen : splen;
+
+            int bsize = bps switch
+            {
+                4 => fnsplen / 2,
+                8 => fnsplen * sizeof(byte),
+                10 => ((fnsplen * 5 + 3) / 4),
+                12 => ((fnsplen * 3) / 2),
+                16 => fnsplen * sizeof(short),
+                24 => fnsplen * 3,
+                32 => fnsplen * sizeof(float),
+                _ => 0
+            };
+
+            int bitm = sampletype switch
+            {
+                1 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                2 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 2,
+                3 => Marshal.SizeOf<AAFC_HEADER>() + fnsplen / 8,
+                4 => Marshal.SizeOf<AAFC_HEADER>() + bsize,
+                _ => 0,
+            };
+
+            byte[] rst = new byte[bitm];
+
+            Marshal.Copy((IntPtr)rstptr, rst, 0, bitm);
+            Marshal.FreeHGlobal((IntPtr)rstptr);
+            return rst;
+        }
+
         public unsafe static AudioClip FromByteArray(byte[] bytes, string n)
         {
-            IntPtr hptr = aafc_getheader(bytes);
-            int freq = 0;
-            int channels = 0;
-            int samplelength = 0;
-
-            if (hptr != IntPtr.Zero)
+            fixed (byte* bptr = bytes)
             {
-                if (bytes[0] == 'A' && bytes[1] == 'A' && bytes[2] == 'F' && bytes[3] == 'C')
+                IntPtr hptr = aafc_getheader(bptr);
+                int freq, channels, samplelength = 0;
+
+                if (hptr != IntPtr.Zero)
                 {
-                    AAFC_HEADER header = Marshal.PtrToStructure<AAFC_HEADER>(hptr);
-                    freq = header.freq; channels = header.channels; samplelength = header.samplelength;
+                    if (bptr[0] == 'A' && bptr[1] == 'A' && bptr[2] == 'F' && bptr[3] == 'C')
+                    {
+                        AAFC_HEADER header = Marshal.PtrToStructure<AAFC_HEADER>(hptr);
+                        freq = header.freq; channels = header.channels; samplelength = header.samplelength;
+                    }
+                    else
+                    {
+                        int* iptr = (int*)hptr;
+                        freq = *iptr;
+                        channels = *(iptr + 1);
+                        samplelength = *(iptr + 2);
+                    }
                 }
                 else
                 {
-                    int* iptr = (int*)hptr;
-                    freq = *iptr;
-                    channels = *(iptr + 1);
-                    samplelength = *(iptr + 2);
+                    Console.WriteLine("Invalid header! It must be AAFC data.");
+                    return null;
                 }
+                IntPtr samples = aafc_import(bptr);
+                AudioClip clip = new(n, (float*)samples, samplelength / channels, freq, channels);
+                aafc_free_bytes(bptr)
+                return clip;
             }
-            else
-            {
-                Console.WriteLine("Invalid header! It must be AAFC data.");
-                return null;
-            }
-
-            IntPtr samples = aafc_import(bytes);
-
-            AudioClip clip = new(n, (float*)samples, samplelength / channels, freq, channels);
-
-            return clip;
         }
 
         // read aafc from file system
